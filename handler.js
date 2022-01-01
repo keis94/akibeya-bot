ce89d2ab16572ee37ce067955022b976af6e4ba0
keis94 <keis.vivi@gmail.com>
2022-01-02 03:39:30 +0900

    Add serverless configuration and functions

---
+++ handler.js
@@ -0,0 +1,51 @@
+'use strict'
+const axios = require('axios').default
+const cheerio = require('cheerio')
+
+module.exports.run = async (event, context) => {
+  const body = new URLSearchParams({
+    cimmotool_status: 1, // Available
+  })
+  const response = await axios.post(
+    'http://reservation.livingscience.ch/en/dynasite.cfm?cmd=cimmotool_immotool_immotool_search&dsmid=509228&skipfurl=1',
+    body
+  )
+
+  const doc = cheerio.load(response.data)
+  const output = doc(
+    '#cimmotool_immotool_immotool_search > div.list.scroll > div'
+  )
+    .text()
+    .trim()
+
+  if (output === 'No record available') {
+    console.log('No available room')
+    return
+  }
+
+  console.log('Available rooms found')
+  await axios.post(
+    'https://api.line.me/v2/bot/message/push',
+    {
+      to: `${process.env.LINE_GROUP_ID}`,
+      messages: [
+        {
+          type: 'text',
+          text: `空き部屋が出ました $\nアクセスして確認 ↓ http://reservation.livingscience.ch/en/living`,
+          emojis: [
+            {
+              index: 10,
+              productId: '5ac22a8c031a6752fb806d66',
+              emojiId: '092',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.LINE_CHANNEL_ACCESS_TOKEN}`,
+      },
+    }
+  )
+}
